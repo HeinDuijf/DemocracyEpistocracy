3284a2b3c79f18ef74843b620c848842d81bd229
HeinDuijf <duijf.hein@gmail.com>
2026-04-15 16:03:19 +0200

    revisions for aggregative crowds

--- models/generate_teams.py
+++ models/generate_teams.py
@@ -10,7 +10,7 @@ from models.team import Team
 from utils.basic_functions import calculate_diversity
 
 
-def generate_expert_team(sources: Sources, heuristic_size: int | list, team_size: int):
+def generate_expert_team(sources: Sources, heuristic_size: int | list, size: int):
     if not isinstance(heuristic_size, list):
         heuristic_size = [heuristic_size]
 
@@ -23,11 +23,11 @@ def generate_expert_team(sources: Sources, heuristic_size: int | list, team_size
 
     agent_score_tuples = [[agent, agent.score] for agent in possible_agents]
     agent_score_tuples.sort(key=lambda item: item[1], reverse=True)
-    best_agents = [agent for [agent, _] in agent_score_tuples[:team_size]]
+    best_agents = [agent for [agent, _] in agent_score_tuples[:size]]
     return Team(best_agents, sources)
 
 
-def generate_diverse_team(sources: Sources, heuristic_size: int | list, team_size: int):
+def generate_diverse_team(sources: Sources, heuristic_size: int | list, size: int):
     if not isinstance(heuristic_size, list):
         heuristic_size = [heuristic_size]
 
@@ -40,7 +40,7 @@ def generate_diverse_team(sources: Sources, heuristic_size: int | list, team_siz
 
     diversity_dict: dict[Agent, float] = {agent: 0 for agent in possible_agents}
     diverse_group = []
-    for _ in range(team_size):
+    for _ in range(size):
         max_diversity = max(diversity_dict.values())
         new_member = rd.choice(
             [
@@ -58,7 +58,13 @@ def generate_diverse_team(sources: Sources, heuristic_size: int | list, team_siz
     return Team(diverse_group, sources)
 
 
-def generate_random_team(sources: Sources, heuristic_size: int | list, team_size: int):
+def generate_random_crowd(
+    sources: Sources,
+    heuristic_size: int | list,
+    size: int,
+    qualifying_percentile: float = 0.0,
+    # without_replacement: bool = False,
+):
     if not isinstance(heuristic_size, list):
         heuristic_size = [heuristic_size]
 
@@ -68,15 +74,20 @@ def generate_random_team(sources: Sources, heuristic_size: int | list, team_size
     possible_agents = [
         Agent(id, heuristic, sources) for id, heuristic in enumerate(all_heuristics)
     ]
+    agent_scores = [agent.score for agent in possible_agents]
+    qualifying_score = np.percentile(agent_scores, qualifying_percentile)
+    qualified_agents = [
+        agent for agent in possible_agents if agent.score >= qualifying_score
+    ]
 
-    random_group = rd.sample(possible_agents, team_size)
+    random_group = rd.sample(qualified_agents, size)
     return Team(random_group, sources)
 
 
 def generate_restricted_team(
     sources: Sources,
     heuristic_size: int | list,
-    team_size: int,
+    size: int,
     qualifying_percentile: float,
     without_replacement: bool = False,
 ):
@@ -98,7 +109,7 @@ def generate_restricted_team(
 
     diversity_dict: dict[Agent, float] = {agent: 0 for agent in qualified_agents}
     diverse_group = []
-    for _ in range(team_size):
+    for _ in range(size):
         max_diversity = max(diversity_dict.values())
         new_member = rd.choice(
             [
@@ -121,7 +132,7 @@ if __name__ == "__main__":
     sources = Sources(n_sources=13)
     start = time.time()
     team = generate_restricted_team(
-        sources, heuristic_size=5, team_size=9, qualifying_percentile=90
+        sources, heuristic_size=5, size=9, qualifying_percentile=90
     )
     mid = time.time()
     print(f"Accuracy team: {team.accuracy_opinion()}")
