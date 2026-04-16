1d7a2510b7b50fc57647117e18c2134b759867f9
HeinDuijf <duijf.hein@gmail.com>
2026-04-16 22:50:13 +0200

    new produce_df method tailored to new format csv

---
+++ figures/generate_df.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import pandas as pd
+
+DATA_DIR = Path(__file__).parent.parent / "data"
+
+
+def produce_df_results(
+    procedure_list: list[str] = ["deliberation", "aggregation"],
+    heuristic_size: int | list[int] = 5,
+    reliability_range: float = 0.2,
+    n_sources_list: list[int] = [13, 17],
+    date: str = "",
+) -> pd.DataFrame:
+    files = [
+        file
+        for file in DATA_DIR.iterdir()
+        if file.stem.split("_")[0] == "simulation"
+        and date in file.stem.split("_")[1] + file.stem.split("_")[2]
+    ]
+    heuristic_str: str | int = heuristic_size  # type: ignore
+    if isinstance(heuristic_size, list):
+        heuristic_str = str(heuristic_str)[1:-1].replace(", ", "-")  # type: ignore
+
+    dfs = []
+    for file in files:
+        df = pd.read_csv(file, index_col=0)
+        if (
+            heuristic_str in df.heuristic_size.values
+            and reliability_range in df.reliability_range.values
+        ):
+            df = df[df["procedure"].isin(procedure_list)]
+            df = df[df["n_sources"].isin(n_sources_list)]
+            dfs.append(df)
+    return pd.concat(dfs, ignore_index=True)
