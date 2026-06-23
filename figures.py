9b4c3cc6e55ed1ba6b2c3bdfeef6c53314d95a66
Hein Duijf <duijf.hein@gmail.com>
2026-06-23 09:34:58 +0200

    constrained params whiskerplot

--- figures.py
+++ figures.py
@@ -3,12 +3,16 @@ from figures.generate_whiskerplot import produce_whiskers_plot
 from figures.individual_scores import boxplot_individual_scores
 
 if __name__ == "__main__":
-    colors = True
-    heatmap(procedure="deliberation", colors=colors)
-    produce_whiskers_plot(filename="figures/images/whiskerplot_all")
+    colors = False
+    # heatmap(procedure="deliberation", colors=colors)
     produce_whiskers_plot(
-        n_sources_list=[13],
-        rel_mean_list=[0.55],
-        filename="figures/images/whiskerplot_13_55",
+        filename="figures/images/whiskerplot_all",
+        n_sources_list=[13, 17],
+        rel_mean_list=[0.55, 0.6],
     )
-    boxplot_individual_scores()
+    # produce_whiskers_plot(
+    #     n_sources_list=[13],
+    #     rel_mean_list=[0.55],
+    #     filename="figures/images/whiskerplot_13_55",
+    # )
+    # boxplot_individual_scores()
